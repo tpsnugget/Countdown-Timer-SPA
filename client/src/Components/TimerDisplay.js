adec0ebb88ed8a60092be1935dd6ea15251b7b79
Mike Giebner <mikegiebner@gmail.com>
2020-07-20 19:31:44 -0400

    HH : SS format is now fixed

--- client/src/Components/TimerDisplay.js
+++ client/src/Components/TimerDisplay.js
@@ -9,6 +9,8 @@ import ControlButtons from '../Components/ControlButtons'
 
 const TimerDisplay = props => {
 
+    var countDownDisplay = ''
+
     const { minute, start } = props
 
     const [halfway, setHalfway] = useState(0)
@@ -20,11 +22,11 @@ const TimerDisplay = props => {
     const [onePointFiveXButton, setOnePointFiveXButton] = useState(false)
     const [twoXButton, setTwoXButton] = useState(false)
     const [keepRunning, setKeepRunning] = useState(true)
-    
-    useEffect( () => {
 
-        if(start && sec === 0){
-            if(halfway === 0){
+    useEffect(() => {
+
+        if (start && sec === 0) {
+            if (halfway === 0) {
                 console.log('halfway === 0')
                 setPauseCount(false)
                 setTotalSec(minute * 60)
@@ -32,63 +34,63 @@ const TimerDisplay = props => {
             }
         }
 
-        if(!pauseCount && oneXButton){
-            if(start && (totalSec % 60 === 0)){
-                setTimeout( () => {
+        if (!pauseCount && oneXButton) {
+            if (start && (totalSec % 60 === 0)) {
+                setTimeout(() => {
                     setMin((totalSec / 60) - 1)
                     setSec(59)
                     setTotalSec(totalSec - 1)
                     console.log('1X setTimeout and totalSec is ', totalSec)
-                }, 1000 )
+                }, 1000)
             } else {
-                setTimeout( () => {
+                setTimeout(() => {
                     setSec(sec - 1)
                     setTotalSec(totalSec - 1)
                     console.log('1X setTimeout and totalSec is ', totalSec)
-                }, 1000 )
+                }, 1000)
             }
         }
-        else if(!pauseCount && onePointFiveXButton){
-            if(start && (totalSec % 60 === 0)){
-                setTimeout( () => {
+        else if (!pauseCount && onePointFiveXButton) {
+            if (start && (totalSec % 60 === 0)) {
+                setTimeout(() => {
                     setMin((totalSec / 60) - 1)
                     setSec(59)
                     setTotalSec(totalSec - 1)
                     console.log('1X setTimeout and totalSec is ', totalSec)
-                }, 750 )
+                }, 750)
             } else {
-                setTimeout( () => {
+                setTimeout(() => {
                     setSec(sec - 1)
                     setTotalSec(totalSec - 1)
                     console.log('1X setTimeout and totalSec is ', totalSec)
-                }, 750 )
+                }, 750)
             }
         }
-        else if(!pauseCount && twoXButton){
-            if(start && (totalSec % 60 === 0)){
-                setTimeout( () => {
+        else if (!pauseCount && twoXButton) {
+            if (start && (totalSec % 60 === 0)) {
+                setTimeout(() => {
                     setMin((totalSec / 60) - 1)
                     setSec(59)
                     setTotalSec(totalSec - 1)
                     console.log('1X setTimeout and totalSec is ', totalSec)
-                }, 500 )
+                }, 500)
             } else {
-                setTimeout( () => {
+                setTimeout(() => {
                     setSec(sec - 1)
                     setTotalSec(totalSec - 1)
                     console.log('1X setTimeout and totalSec is ', totalSec)
-                }, 500 )
+                }, 500)
             }
         }
 
-        if(totalSec === 0){
+        if (totalSec === 0) {
             setKeepRunning(false)
             setPauseCount(true)
             setMin(0)
             setSec(0)
         }
 
-    } )
+    })
 
     const runAtOneSpeed = async () => {
         await setOneXButton(true)
@@ -110,49 +112,76 @@ const TimerDisplay = props => {
 
     console.log('TimerDisplay Rendered and totalSec is ', totalSec)
 
-    return (
-        <Grid container justify='center' alignItems='center' style={{marginTop: '3rem'}} >
-            <Grid container justify='center' item xs={12}>
-                {keepRunning && start && (totalSec <= halfway) && (totalSec > 20) ?
-                <Typography variant='h6' >
-                    More than halfway there!
+    // This section handles the MM : SS formatting
+    if (min < 10 && sec < 10) {
+        countDownDisplay = (
+            <Grid item xs={4}>
+                <Typography variant='h1' data-testid='Timerdisplay-time' >
+                    0{min} : 0{sec}
                 </Typography>
-                : null}
-                {keepRunning && start && (totalSec <= halfway) && (totalSec <= 20) ?
-                <Typography variant='h6' style={{color: 'red'}} >
-                    More than halfway there!
-                </Typography>
-                : null}
-                {!keepRunning ?
-                <Typography variant='h6' >
-                    Time's Up!
-                </Typography>
-                : null}
             </Grid>
-            <Grid container justify='center' alignItems='center'>
-                {sec < 10 ?
-                <Grid item xs={4}>
-                    <Typography variant='h1'data-testid='Timerdisplay-time' >
-                        0{min} : 0{sec}
-                    </Typography>
-                </Grid>
-                :
-                <Grid item xs={4}>
-                <Typography variant='h1'data-testid='Timerdisplay-time' >
+        )
+    }
+    else if (min >= 10 && sec < 10) {
+        countDownDisplay = (
+            <Grid item xs={4}>
+                <Typography variant='h1' data-testid='Timerdisplay-time' >
+                    {min} : 0{sec}
+                </Typography>
+            </Grid>
+        )
+    }
+    else if (min >= 10 && sec >= 10) {
+        countDownDisplay = (
+            <Grid item xs={4}>
+                <Typography variant='h1' data-testid='Timerdisplay-time' >
                     {min} : {sec}
                 </Typography>
-                </Grid>
-                }
+            </Grid>
+        )
+    } else {
+        countDownDisplay = (
+            <Grid item xs={4}>
+                <Typography variant='h1' data-testid='Timerdisplay-time' >
+                    0{min} : {sec}
+                </Typography>
+            </Grid>
+        )
+    }
+
+    return (
+        <Grid container justify='center' alignItems='center' style={{ marginTop: '3rem' }} >
+            <Grid container justify='center' item xs={12}>
+                {keepRunning && start && (totalSec <= halfway) && (totalSec > 20) ?
+                    <Typography variant='h6' >
+                        More than halfway there!
+                </Typography>
+                    : null}
+                {keepRunning && start && (totalSec <= halfway) && (totalSec <= 20) ?
+                    <Typography variant='h6' style={{ color: 'red' }} >
+                        More than halfway there!
+                </Typography>
+                    : null}
+                {!keepRunning ?
+                    <Typography variant='h6' >
+                        Time's Up!
+                </Typography>
+                    : null}
+            </Grid>
+            <Grid container justify='center' alignItems='center'>
+
+            {countDownDisplay}
+            
                 <Grid item xs={1}>
-                {pauseCount ?
-                    <Button>
-                        <PlayCircleOutlineIcon style={{fontSize: '4rem'}} onClick={() => {setPauseCount(false)}} />
-                    </Button>
-                    :
-                    <Button data-testid='button' >
-                        <PauseCircleOutlineIcon style={{fontSize: '4rem'}} onClick={() => {setPauseCount(true)}} />
-                    </Button>
-                }
+                    {pauseCount ?
+                        <Button>
+                            <PlayCircleOutlineIcon style={{ fontSize: '4rem' }} onClick={() => { setPauseCount(false) }} />
+                        </Button>
+                        :
+                        <Button data-testid='button' >
+                            <PauseCircleOutlineIcon style={{ fontSize: '4rem' }} onClick={() => { setPauseCount(true) }} />
+                        </Button>
+                    }
                 </Grid>
             </Grid>
             <ControlButtons oneXButton={oneXButton} onePointFiveXButton={onePointFiveXButton} twoXButton={twoXButton} runAtOne={runAtOneSpeed} runAtOnePointFive={runAtOnePointFiveSpeed} runAtTwo={runAtTwoSpeed} />
